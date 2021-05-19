6e9fac27b1c47a6ebfc223f440f546aed15a488e
Jonathan Diaz <jonaalbert.adm@gmail.com>
2021-05-18 21:09:29 -0400

    Creacion proyectos

---
+++ AnagramsTest/ManageAnagramsTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Anagrams;
+
+namespace AnagramsTest
+{
+    public class ManageAnagramsTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
